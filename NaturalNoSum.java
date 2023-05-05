d99cd27e832a378f7e5b15685a190423603405e0
Ayush14Mathur <128884030+Ayush14Mathur@users.noreply.github.com>
2023-05-05 18:13:07 +0530

    Update NaturalNoSum.java

--- NaturalNoSum.java
+++ NaturalNoSum.java
@@ -2,7 +2,7 @@ public class NaturalNoSum {
 
 public static void main(String[]args) {
 	int n,sum=0;
-	for(n=1; n<=10; n++)
+	for(n=1; n<=20; n++)
 		sum = sum + n;
 	System.out.println("Sum =" +sum);
 }
